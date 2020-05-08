6523bcd28f4b233a1febc0036df6f7d7c1cbddf1
sugyeong Kim <s2018s25@e-mirim.hs.kr>
2020-05-08 19:32:25 +0900

    Thread 활용 예제

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace csloop
 {
@@ -10,13 +11,14 @@ namespace csloop
     {
         static void Main(string[] args)
         {
-            long start = DateTime.Now.Ticks;
+            /*long start = DateTime.Now.Ticks;
             long count = 0;
             while (start + (10000000) > DateTime.Now.Ticks)
             {
                 count++;
             }
             Console.WriteLine(count + "만큼 반복함");
+            */
 
             string[] array = { "사과", "배", "포도", "딸기", "바나나" };
             foreach(string item in array)
@@ -28,6 +30,25 @@ namespace csloop
             {
                 Console.WriteLine(item);
             }
+
+
+            int x = 1;
+            while (x < 50)
+            {
+                Console.Clear(); // 화면 지운 뒤 커서 이동
+                Console.SetCursorPosition(x, 5);
+
+                if (x % 3 == 0)
+                    Console.WriteLine("__@");
+                else if (x % 3 == 1)
+                    Console.WriteLine("_^@");
+                else
+                    Console.WriteLine("^_@");
+
+                Thread.Sleep(100); // 100밀리초 정지 후 x++
+                x++;
+            }
+
         }
     }
 }
